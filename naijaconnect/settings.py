46cdeeb2f13298dc4958cccc14f21fd94d6a7cbb
dolastack <india@ubuntu.dolastack.com>
2017-06-21 13:03:10 -0400

    celery app added

--- naijaconnect/settings.py
+++ naijaconnect/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'news.apps.NewsConfig',
     'clips.apps.ClipsConfig',
+    'djcelery',
+    'kombu.transport.django',
 ]
 
 MIDDLEWARE = [
