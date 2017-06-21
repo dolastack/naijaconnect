9ce02ff55a3a5e32072864c582d3fd0554143aeb
dolastack <india@ubuntu.dolastack.com>
2017-06-21 13:09:59 -0400

    changes in celery.py

--- naijaconnect/celery.py
+++ naijaconnect/celery.py
@@ -5,7 +5,7 @@ from django.conf import settings
 # Indicate Celery to use the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'naijaconnect.settings')
 
-app = Celery('rssfeed')
+app = Celery('naijaconnect')
 app.config_from_object('django.conf:settings')
 # This line will tell Celery to autodiscover all your tasks.py that are in your app folders
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
